d27ce765ca5b9f3f1c2e109363b5d7b65de437be
Fabricio <Fabricio@FABRICIO>
2016-11-22 23:33:13 -0200

    Add project files.

---
+++ Teste/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Teste
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Moeda(ref textBox1);
+        }
+
+        public static void Moeda(ref TextBox txt)
+        {
+            //https://www.youtube.com/watch?v=mqwhgCSOTdA
+
+            string n = string.Empty;
+            double v = 0;
+            try
+            {
+                n = txt.Text.Replace(",", "").Replace(".", "");
+                if (n.Equals(""))
+                    n = "";
+                n = n.PadLeft(3, '0');
+                if (n.Length > 3 & n.Substring(0, 1) == "0")
+                    n = n.Substring(1, n.Length - 1);
+                v = Convert.ToDouble(n) / 100;
+                txt.Text = string.Format("{0:N}", v);
+                txt.SelectionStart = txt.Text.Length;
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double tes = Convert.ToDouble(textBox1.Text);
+
+            MessageBox.Show(Convert.ToString(tes));
+        }
+    }
+}
